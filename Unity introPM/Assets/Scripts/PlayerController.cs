fa667f9adb790522954811dbcbbc238e05220b58
4Forest1 <runareana1001@gmail.com>
2024-09-09 13:38:49 -0700

    Movement

---
+++ Unity introPM/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody myRB;
+
+    public float speed = 10.0f;
+    public float jumpHeight = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       myRB = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = myRB.velocity;
+
+        temp.x = Input.GetAxisRaw("Vertical") * speed;
+        temp.z = Input.GetAxisRaw("Horizontal") * speed;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            temp.y = jumpHeight;
+        }
+
+        myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
+    }
+}
