0a2489cb946c1a75e487331c94234a02eeac521e
4Forest1 <runareana1001@gmail.com>
2024-09-10 13:44:51 -0700

    Jump, sprint and camera

--- Unity introPM/Assets/Scripts/PlayerController.cs
+++ Unity introPM/Assets/Scripts/PlayerController.cs
@@ -5,24 +5,79 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody myRB;
+    Camera PlayerCam;
 
+    Vector2 camRotation;
+
+    public bool sprintMode = false;
+
+    [Header("Movement Settings")]
     public float speed = 10.0f;
-    public float jumpHeight = 10.0f;
+    public float sprintMultiplier = 2.5f;
+    public float jumpHeight = 5.0f;
+    public float groundDetectDistance = 1.5f;
+
+
+    [Header("User Settings")]
+    public bool sprintToggleOption = false;
+    public float mouseSensitivity = 2.0f;
+    public float Xsensitivity = 2.0f;
+    public float Ysensitivity = 2.0f;
+    public float camRotationLimit = 90f;
     // Start is called before the first frame update
     void Start()
     {
-       myRB = GetComponent<Rigidbody>(); 
+       myRB = GetComponent<Rigidbody>();
+       PlayerCam = transform.GetChild(0).GetComponent<Camera>();
+
+       camRotation = Vector2.zero;
+       Cursor.visible = false;
+       Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+        camRotation.y = Mathf.Clamp(camRotation.y, -camRotationLimit, camRotationLimit);
+
+        PlayerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
+
         Vector3 temp = myRB.velocity;
 
-        temp.x = Input.GetAxisRaw("Vertical") * speed;
+        if(!sprintToggleOption)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+                sprintMode = true;
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+                sprintMode = false;
+        }
+
+        if(sprintToggleOption)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetAxisRaw("Vertical") > 0)
+                sprintMode = true;
+
+            if (Input.GetAxisRaw("Vertical") <= 0)
+                sprintMode = false;
+        }
+
+
+
+        if (!sprintMode)
+            temp.x = Input.GetAxisRaw("Vertical") * speed;
+
+        if (sprintMode)
+            temp.x = Input.GetAxisRaw("Vertical") * speed * sprintMultiplier;
+
         temp.z = Input.GetAxisRaw("Horizontal") * speed;
+
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, groundDetectDistance))
         {
             temp.y = jumpHeight;
         }
